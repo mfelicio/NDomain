340e9250befd56f4f85ddef9608267dd5b75e6d1
James Holcomb <james.holcomb@gmail.com>
2015-09-23 16:10:25 -0500

    Add Serilog loging support

---
+++ source/NDomain.Serilog/LogFactory.cs
@@ -0,0 +1,18 @@
+﻿using NDomain.Logging;
+using System;
+
+namespace NDomain.Serilog
+{
+	public class LoggerFactory : ILoggerFactory
+	{
+		public ILogger GetLogger(string name)
+		{
+			return new Logger(global::Serilog.Log.Logger.ForContext("SourceContext", name));
+		}
+
+		public ILogger GetLogger(Type type)
+		{
+			return new Logger(global::Serilog.Log.Logger.ForContext(type));
+		}
+	}
+}
---
+++ source/NDomain.Serilog/Logger.cs
@@ -0,0 +1,55 @@
+﻿using NDomain.Logging;
+using System;
+
+namespace NDomain.Serilog
+{
+	public class Logger : ILogger
+	{
+		global::Serilog.ILogger _logger;
+
+		public Logger(global::Serilog.ILogger logger)
+		{
+			_logger = logger;
+		}
+
+		public void Debug(string message, params object[] args)
+		{
+			_logger.Debug(message, args);
+		}
+
+		public void Info(string message, params object[] args)
+		{
+			_logger.Information(message, args);
+		}
+
+		public void Warn(string message, params object[] args)
+		{
+			_logger.Warning(message, args);
+		}
+
+		public void Warn(Exception exception, string message, params object[] args)
+		{
+			_logger.Warning(exception, string.Format(message, args));
+		}
+
+		public void Error(string message, params object[] args)
+		{
+			_logger.Error(message, args);
+		}
+
+		public void Error(Exception exception, string message, params object[] args)
+		{
+			_logger.Error(exception, message, args);
+		}
+
+		public void Fatal(string message, params object[] args)
+		{
+			_logger.Fatal(message, args);
+		}
+
+		public void Fatal(Exception exception, string message, params object[] args)
+		{
+			_logger.Fatal(exception, string.Format(message, args));
+		}
+	}
+}
