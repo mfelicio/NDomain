eac3e0254f59d897e387002909d589a0e0dd1521
James Holcomb <james.holcomb@gmail.com>
2015-09-24 09:29:23 -0500

    Use 'this' convention in Serilog.Logger

--- source/NDomain.Serilog/Logger.cs
+++ source/NDomain.Serilog/Logger.cs
@@ -5,51 +5,51 @@ namespace NDomain.Serilog
 {
 	public class Logger : ILogger
 	{
-		global::Serilog.ILogger _logger;
+		global::Serilog.ILogger logger;
 
 		public Logger(global::Serilog.ILogger logger)
 		{
-			_logger = logger;
+			this.logger = logger;
 		}
 
 		public void Debug(string message, params object[] args)
 		{
-			_logger.Debug(message, args);
+			this.logger.Debug(message, args);
 		}
 
 		public void Info(string message, params object[] args)
 		{
-			_logger.Information(message, args);
+			this.logger.Information(message, args);
 		}
 
 		public void Warn(string message, params object[] args)
 		{
-			_logger.Warning(message, args);
+			this.logger.Warning(message, args);
 		}
 
 		public void Warn(Exception exception, string message, params object[] args)
 		{
-			_logger.Warning(exception, string.Format(message, args));
+			this.logger.Warning(exception, string.Format(message, args));
 		}
 
 		public void Error(string message, params object[] args)
 		{
-			_logger.Error(message, args);
+			this.logger.Error(message, args);
 		}
 
 		public void Error(Exception exception, string message, params object[] args)
 		{
-			_logger.Error(exception, message, args);
+			this.logger.Error(exception, message, args);
 		}
 
 		public void Fatal(string message, params object[] args)
 		{
-			_logger.Fatal(message, args);
+			this.logger.Fatal(message, args);
 		}
 
 		public void Fatal(Exception exception, string message, params object[] args)
 		{
-			_logger.Fatal(exception, string.Format(message, args));
+			this.logger.Fatal(exception, string.Format(message, args));
 		}
 	}
 }
