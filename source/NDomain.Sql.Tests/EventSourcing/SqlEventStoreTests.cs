d07e4b82e7caafab681c25d772a95f660a8e4205
Manuel Felicio <manuelfelicio@gmail.com>
2017-05-20 12:11:31 +0100

    added SqlEventStoreTests which implements the EventStore specs

---
+++ source/NDomain.Sql.Tests/EventSourcing/SqlEventStoreTests.cs
@@ -0,0 +1,80 @@
+﻿using NDomain.Tests.Specs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NDomain.EventSourcing;
+using NUnit.Framework;
+using System.Data.SqlClient;
+using NDomain.Sql.EventSourcing;
+using System.Configuration;
+using Dapper;
+
+namespace NDomain.Sql.Tests.EventSourcing
+{
+    [TestFixture]
+    [Category("integration")]
+    public class SqlEventStoreTests : EventStoreSpecs
+    {
+        private readonly string _connectionString;
+        private readonly SqlEventStoreDb _store;
+        private readonly SqlObjectNames _sqlNames;
+
+        public SqlEventStoreTests()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["EventStore.Database"].ConnectionString;
+            _sqlNames = new SqlObjectNames("atest", "Aggregates", "Events");
+            _store = new SqlEventStoreDb(_connectionString, _sqlNames);
+        }
+
+        protected override IEventStoreDb CreateEventStorage()
+        {
+            return _store;
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _store.Init();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            string oneTimeTearDownString = @"
+                drop table {0}.{1}
+                drop table {0}.{2}
+                drop schema {0}";
+
+            string cmd = string.Format(
+                oneTimeTearDownString,
+                _sqlNames.SchemaName,
+                _sqlNames.AggregateTableName,
+                _sqlNames.EventTableName);
+
+            using (var con = new SqlConnection(_connectionString))
+            {
+                con.Execute(cmd);
+            }
+        }
+
+        protected override void OnTearDown()
+        {
+            string tearDownString = @"
+                DELETE FROM {0}.{1}
+                DELETE FROM {0}.{2}";
+
+            string cmd = string.Format(
+                tearDownString,
+                _sqlNames.SchemaName,
+                _sqlNames.AggregateTableName,
+                _sqlNames.EventTableName);
+
+            using (var con = new SqlConnection(_connectionString))
+            {
+                con.Execute(cmd);
+            }
+        }
+    }
+}
